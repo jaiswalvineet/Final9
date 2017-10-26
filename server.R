83b6a9ba24c0665429a24d2c6580f0d27127966c
SAPIENT\vjais1 <vjaiswal@sapient.com>
2017-10-26 15:39:09 +0530

    First one

---
+++ server.R
@@ -0,0 +1,39 @@
+
+# This is the server logic for a Shiny web application.
+# You can find out more about building applications with Shiny here:
+#
+# http://shiny.rstudio.com
+#
+
+library(shiny)
+
+# Load the raw data
+rawData <- read.csv("raw.csv", stringsAsFactors = F)
+
+shinyServer(function(input, output) {
+
+  output$distPlot <- renderPlot({
+
+    # generate bins based on input$bins from ui.R
+    x    <- rawData$budget
+    bins <- seq(min(x), max(x), length.out = input$bins + 1)
+
+    # draw the histogram with the specified number of bins
+    hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    
+    output$Table <- renderTable({
+        output<-data.frame(genres)
+        output
+    })
+
+  })
+
+})
+
+
+## table name 
+  genre <- raw$genres
+  genreList <- lapply(genre,function(x) fromJSON(x))
+  genreList <- genreList[sapply(genreList, function(x) as.numeric(dim(x)[1])) > 0]
+  genreList <- genreList[!sapply(genreList, is.null)]
+  genres <- unique(Reduce(union, genreList)) 
---
+++ ui.R
@@ -0,0 +1,47 @@
+
+# This is the user-interface definition of a Shiny web application.
+# You can find out more about building applications with Shiny here:
+#
+# http://shiny.rstudio.com
+#
+
+library(shiny)
+
+shinyUI(fluidPage(
+
+  # Application title
+  titlePanel("Old Faithful Geyser Data"),
+
+  # Sidebar with a slider input for number of bins
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput("bins",
+                  "Number of bins:",
+                  min = 1,
+                  max = 50,
+                  value = 30),
+      sliderInput("budget",
+                  "Select the budget:",
+                  min = 0,
+                  max = 380000000,
+                  value = 100000),
+      selectInput("Genre", "Choose a genre:",
+                  list(`East Coast` = c("NY", "NJ", "CT"),
+                       `West Coast` = c("WA", "OR", "CA"),
+                       `Midwest` = c("MN", "WI", "IA"))
+      )
+    ),
+
+    # Show a plot of the generated distribution
+    mainPanel(
+      plotOutput("distPlot"),
+      tabsetPanel(
+        tabPanel("Main",
+                 htmlOutput("header"),
+                 tableOutput("Table"),
+                 htmlOutput("Text")
+        )
+      )
+    )
+  )
+))
